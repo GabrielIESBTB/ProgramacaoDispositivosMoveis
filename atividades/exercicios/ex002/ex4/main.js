2f4ec607493494fcac8f3a07d816432e0a7a0d29
GabrielIESBTB <ggomes@iesb.edu.br>
2025-03-08 00:09:25 -0300

    .

--- atividades/exercicios/ex002/ex4/main.js
+++
@@ -1,9 +0,0 @@
-
-function imprimirNumeros() {
-  console.log("Números de 1 a 100:");
-  for (let i = 1; i <= 100; i++) {
-    console.log(i);
-  }
-}
-
-imprimirNumeros();
--- atividades/exercicios/ex002/ex5/main.js
+++
@@ -1,9 +0,0 @@
-
-function operacoesNumero(numero) {
-  console.log("Número:", numero);
-  console.log("Dobro:", numero * 2);
-  console.log("Triplo:", numero * 3);
-  console.log("Quadrado:", numero * numero);
-}
-
-operacoesNumero(5);
